7b7a6c52dc6db3e15df36bbb9b7ed9d5687b28ab
Rich Beredo <richard.s.beredo.ctr@army.mil>
2023-09-12 15:33:48 -0400

    maybe this works?

--- client/src/DownloadResume.tsx
+++ client/src/DownloadResume.tsx
@@ -1,21 +1,36 @@
 import React from 'react'
+// import file from './assets/Beredo_Resume.docx'
 
-const DownloadResume = async () => {
-    try {
-        const response = await fetch('/resume');
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'Beredo_Resume.docx';
-        a.style.display = 'none';
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
-    } catch (error) {
-        console.error(error)
-    }
+// const DownloadResume = async () => {
+
+//     console.log("output:", file)
+
+// }
+import file from './assets/Beredo_Resume.docx';
+
+const DownloadResume = () => {
+    const a = document.createElement('a');
+    a.href = file;
+    a.download = 'Beredo_Resume.docx';
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
 
 export default DownloadResume
+// try {
+//     const response = await fetch('/resume');
+//     const blob = await response.blob();
+//     const url = window.URL.createObjectURL(blob);
+//     const a = document.createElement('a');
+//     a.href = url;
+//     a.download = 'Beredo_Resume.docx';
+//     a.style.display = 'none';
+//     document.body.appendChild(a);
+//     a.click();
+//     window.URL.revokeObjectURL(url);
+//     document.body.removeChild(a);
+// } catch (error) {
+//     console.error(error)
+// }
